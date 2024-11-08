3d7d478edb9ee014292b76a849ce502ce32b07e0
guileite01 <00001132626663sp@al.educacao.sp.gov.br>
2024-11-08 11:49:55 -0300

    Add files via upload

---
+++ minha ferramenta de trabalho/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'roblox',
+    'O que e o roblox?',
+    'roblox e um jogo infantil'
+)
+
+criaCartao(
+    'ingles',
+    'como escreve carro em ingles?',
+    'carro em ingles e car'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'fisica',
+    'oq e um resistor?',
+    'Resistores são dispositivos usados para controlar a passagem de corrente elétrica em circuitos elétricos por meio do efeito Joule, que converte energia elétrica em energia térmica.(RAI)'
+)
